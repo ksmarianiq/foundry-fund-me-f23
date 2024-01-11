13b24b06b4fd94ab91660b78f5f5a6f8267aace1
ksmarianiq <ksmarianiique@gmail.com>
2024-01-11 16:10:24 +0100

    our first commit

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+contract DeployFundMe  is Script {
+    //FundMe fundMe;
+ function run() external returns(FundMe){
+
+    HelperConfig helperConfig = new HelperConfig();
+    // then get the price feed address
+     address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+    vm.startBroadcast();
+    FundMe fundMe = new FundMe(ethUsdPriceFeed);
+    vm.stopBroadcast();
+    return fundMe;
+ }
+}
